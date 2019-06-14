90964fc113021076ca00d0fe023a23b2ffc13efa
rama41222 <dinushankanrg@gmail.com>
2019-06-15 02:22:59 +0530

    feat: database lib and sequelize mssql connection handler

---
+++ src/lib/bootstrap/database/index.js
@@ -0,0 +1,6 @@
+const { connect, sequelize } = require('./sequelize');
+
+module.exports = {
+  connect,
+  sequelize,
+};
---
+++ src/lib/bootstrap/database/sequelize.js
@@ -0,0 +1,34 @@
+const Sequelize = require('sequelize');
+const config = require('./../../../config');
+
+const { database } = config;
+
+const sequelize = new Sequelize(database.name, database.username, database.password, {
+  host: database.host,
+  port: Number(database.port),
+  dialect: database.dialect,
+  operatorsAliases: database.operatorsAliases,
+  logging: database.logging === 1,
+  pool: {
+    max: database.pool.max,
+    min: database.pool.min,
+    acquire: database.pool.acquire,
+    idle: database.pool.idle,
+  },
+});
+
+async function connect() {
+  return sequelize
+    .authenticate()
+    .then(() => {
+      console.log('Database connection has been established successfully.');
+    })
+    .catch((err) => {
+      console.error('Unable to connect to the database:', err);
+    });
+}
+
+module.exports = {
+  connect,
+  sequelize,
+};
