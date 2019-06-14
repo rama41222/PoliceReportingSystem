427740fb9404307c7e82fa723da6f04f7510cde1
rama41222 <dinushankanrg@gmail.com>
2019-06-15 02:53:04 +0530

    feat: server using bootstrap server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+require('dotenv').config();
+const { bootstrap } = require('./lib');
+const { app } = require('./config');
+
+let server;
+
+async function main() {
+  server = await bootstrap();
+  server.listen(app.port, (e) => {
+    if (e) {
+      throw new Error(e.message);
+    }
+    console.info(`Listening on port ${app.port}`);
+  });
+}
+
+main().catch((e) => console.error(e.message));
+module.exports = main;
